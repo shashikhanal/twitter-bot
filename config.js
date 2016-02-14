faf1a86971c0a2bebb3c9411e18d86081083a7f8
shashikhanal <shashikhanal@github.com>
2016-02-15 02:01:20 +0545

    Tokens hidden

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-	consumer_key:         '56DUpQL9Prrv1uWiVsU9d0Lun',
-	consumer_secret:      'xchuF74O5WpkxVFVRV9ceIfNLUww8BtCiGNiFPJKQUpb8P2sMl',
-	access_token:         '4909702593-x1myniZOMgAgeu5QfAVHod7KPSg2YGJxl9JL7cN',
-	access_token_secret:  '3GwbscaPTV5BIPlFXIp1FJrWKL6ObIlWcpwGlRSlgo3Oi'
+	consumer_key:         '****',
+	consumer_secret:      '****',
+	access_token:         '****',
+	access_token_secret:  '****'
 }
